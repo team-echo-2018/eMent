6ff025e80b87529e2cd3304754866777d741f6fc
Sachintha <ravindusachintha53@gmail.com>
2018-08-18 06:58:09 +0530

    update repository file structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './views/platform/components/nav-bar/nav-bar.component';
+import { NavItemComponent } from './views/platform/components/nav-item/nav-item.component';
+import { HomePageComponent } from './views/platform/pages/home-page/home-page.component';
+import { LoginPageComponent } from './views/platform/pages/login-page/login-page.component';
+import { SignupPageComponent } from './views/platform/pages/signup-page/signup-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    NavItemComponent,
+    HomePageComponent,
+    LoginPageComponent,
+    SignupPageComponent
   ],
   imports: [
     BrowserModule
